3a2d1fb4c3236f29c8980fd1c0750e4583ab9acc
Morti <morti.martinez-seara.7e9@itb.cat>
2026-01-12 13:51:40 +0100

    feat: exercices from 1 to 9 finished

--- Program.cs
+++ Program.cs
@@ -4,52 +4,140 @@ class Program
 {
     public static void Main()
     {
-        long factorial;
-        int numero;
-        Console.WriteLine("Dame un número");
-        numero = Int32.Parse(Console.ReadLine());
+        int[] arrayInt = { 1, 2, 3, 4, 5 };
+        int[] arrayInt2 = { -3, 2, -7, 1, -5 };
 
-        DateTime startTime;
-        startTime = DateTime.Now;
+        //Exercici 1
+        Countdown(7);
+        
+        //Exercici 2
+        Console.WriteLine(CountDigits(2550));
+        
+        //Exercici 3
+        Console.WriteLine(AddDigits(5456));
+        
+        //Exercici 4
+        Console.WriteLine(Swap(123456));
 
-        factorial = Factorial(numero);
+        //Exercici 5
+        PrintForwardArray(arrayInt, 0);
+        Console.WriteLine();
 
-        DateTime endTime;
-        endTime = DateTime.Now;
+        PrintBackwardArray(arrayInt, arrayInt.Length - 1);
+        Console.WriteLine();
 
-        TimeSpan duration = endTime - startTime;
+        //Exercici 6
+        Console.WriteLine(FindMax(arrayInt2, 0));
 
-        Console.WriteLine($"El factorial de {numero} es {factorial}");
-        Console.WriteLine($"Time span: {duration}");
-
-        //No recursivo
-        startTime = DateTime.Now;
-
-        factorial = FactorialNoRecursivo(numero);
-
-        endTime = DateTime.Now;
-
-        duration = endTime - startTime;
-
-        Console.WriteLine($"El factorial de {numero} es {factorial}");
-        Console.WriteLine($"Time span: {duration}");
-    }
-    public static long Factorial(int num)
-    {
-        if (num == 0) return 1;
-
-        else {
-            long resultado = num * Factorial(num - 1);
-            return resultado;
-        }
-    }
-    public static long FactorialNoRecursivo(int num)
-    {
-        long resultado = num;
-        for(int i = 1; i < num; i++)
+        //Exercici 7
+        if (IsPalindrome("reconocer"))
         {
-            resultado *= i;
+            Console.WriteLine("Is Palindrome");
         }
-        return resultado;
+        else
+        {
+            Console.WriteLine("Is not Palindrome");
+        }
+
+        //Exercici 8
+        Console.WriteLine(MCD(17, 13));
+
+        //Exercici 9
+        Console.WriteLine(DecimalToBinary(13));
+    }
+    public static int Countdown(int n)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine(n);
+            return 0;
+        }
+        else
+        {
+            Console.WriteLine(n);
+            return Countdown(n - 1);
+        }
+    }
+    public static int CountDigits(int n)
+    {
+
+        if (n < 10)
+        {
+            return 1;
+        }
+        else
+        {
+            return 1 + CountDigits(n / 10);
+        }
+    }
+    public static int AddDigits(int n)
+    {
+        if (n < 10) return n;
+
+        else return (n % 10) + AddDigits(n / 10);
+    }
+    public static int Swap(int n)
+    {
+        return SwapAux(n, 0);
+    }
+    public static int SwapAux(int n, int result)
+    {
+        if (n < 10) return result*10 + n;
+
+        else
+        {
+            result = result * 10 + (n % 10);
+            return SwapAux(n / 10, result);
+        }
+    }
+    public static void PrintForwardArray(int[] arrayInt, int index)
+    {
+        if (index < arrayInt.Length)
+        {
+            Console.Write($"{arrayInt[index]} ");
+            index++;
+            PrintForwardArray(arrayInt, index);
+        }
+    }
+    public static void PrintBackwardArray(int[] arrayInt, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write($"{arrayInt[index]} ");
+            index--;
+            PrintBackwardArray(arrayInt, index);
+        }
+    }
+    public static int FindMax(int[] arrayInt, int index)
+    {
+        if(index == arrayInt.Length - 1) return arrayInt[index];
+
+        int max = FindMax(arrayInt, index + 1);
+
+        return Math.Max(max, arrayInt[index]);
+ 
+    }
+    public static bool IsPalindrome(string word)
+    {
+        if (word.Length == 0 || word.Length == 1) return true;
+        else if (word[0] != word[word.Length-1]) return false;
+        else
+        {
+            return IsPalindrome(word.Substring(1, word.Length-2));
+        }
+    }
+    public static int MCD(int a, int b)
+    {
+        if (b == 0) return a;
+        else
+        {
+            return MCD(b, a % b);
+        }
+    }
+    public static string DecimalToBinary(int n)
+    {
+        if (n == 0) return "";
+
+        return DecimalToBinary(n / 2) + (n % 2).ToString();
     }
 }
